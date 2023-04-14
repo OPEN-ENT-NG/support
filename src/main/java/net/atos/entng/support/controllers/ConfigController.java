d6f28a7384bf5e9f661b3abfafdca3ad6be5f393
CHAUVET <enzo.chauvet@sfr.fr>
2023-04-14 08:52:49 +0200

    feat(config): #CO-1772 route only for superadmin

--- src/main/java/net/atos/entng/support/controllers/ConfigController.java
+++ src/main/java/net/atos/entng/support/controllers/ConfigController.java
@@ -5,13 +5,13 @@ import fr.wseduc.security.ActionType;
 import fr.wseduc.security.SecuredAction;
 import io.vertx.core.http.HttpServerRequest;
 import org.entcore.common.controller.ControllerHelper;
-import org.entcore.common.http.filter.AdminFilter;
 import org.entcore.common.http.filter.ResourceFilter;
+import org.entcore.common.http.filter.SuperAdminFilter;
 
 public class ConfigController extends ControllerHelper {
     @Get("/config")
     @SecuredAction(value = "", type = ActionType.RESOURCE)
-    @ResourceFilter(AdminFilter.class)
+    @ResourceFilter(SuperAdminFilter.class)
     public void getConfig(final HttpServerRequest request) {
         renderJson(request, config);
     }
