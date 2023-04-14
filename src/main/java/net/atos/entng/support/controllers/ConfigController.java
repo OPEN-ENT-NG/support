8aca99ba9249edfec13058c3d88327d33b45b57d
Enzo Chauvet <73693920+Ozen40@users.noreply.github.com>
2023-04-14 09:51:30 +0200

    feat(config): #CO-1772 add route /config (#29)
    * feat(config): #CO-1772 add route /config
    
    * feat(config): #CO-1772 route only for superadmin

---
+++ src/main/java/net/atos/entng/support/controllers/ConfigController.java
@@ -0,0 +1,18 @@
+package net.atos.entng.support.controllers;
+
+import fr.wseduc.rs.Get;
+import fr.wseduc.security.ActionType;
+import fr.wseduc.security.SecuredAction;
+import io.vertx.core.http.HttpServerRequest;
+import org.entcore.common.controller.ControllerHelper;
+import org.entcore.common.http.filter.ResourceFilter;
+import org.entcore.common.http.filter.SuperAdminFilter;
+
+public class ConfigController extends ControllerHelper {
+    @Get("/config")
+    @SecuredAction(value = "", type = ActionType.RESOURCE)
+    @ResourceFilter(SuperAdminFilter.class)
+    public void getConfig(final HttpServerRequest request) {
+        renderJson(request, config);
+    }
+}
