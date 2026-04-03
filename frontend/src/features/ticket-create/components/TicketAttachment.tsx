d7f80e08030cfd0d486e9d0a68309f14d2c1509e
Théo Vogel <contact@theovogel.fr>
2026-04-03 15:20:55 +0200

    fix: INTEG-1413, add error toast if error during attachment upload

--- frontend/src/features/ticket-create/components/TicketAttachment.tsx
+++ frontend/src/features/ticket-create/components/TicketAttachment.tsx
@@ -1,4 +1,4 @@
-import { AddAttachments, Flex } from '@edifice.io/react';
+import { AddAttachments, Flex, useToast } from '@edifice.io/react';
 import { useMemo, useState } from 'react';
 import { type TicketAttachment } from '~/models';
 import { uploadAttachment } from '~/services';
@@ -8,17 +8,33 @@ interface TicketAttachmentProps {
   attachments: TicketAttachment[];
 }
 
+function isFileTooLarge(e: unknown): boolean {
+  return (
+    typeof e === 'object' && e !== null && (e as any).error === 'file.too.large'
+  );
+}
+
 export default function TicketAddAttachment({
   onChange,
   attachments,
 }: TicketAttachmentProps) {
   const [isMutating, setIsMutating] = useState(false);
+  const toast = useToast();
 
   const handleFilesSelected = async (files: File[]) => {
     setIsMutating(true);
     try {
       const uploaded = await Promise.all(files.map(uploadAttachment));
       onChange((prev) => [...prev, ...uploaded]);
+    } catch (error) {
+      console.error('Error uploading attachment:', error);
+      if (isFileTooLarge(error)) {
+        toast.error(
+          'Erreur lors du téléchargement de la pièce jointe : espace disponible insuffisant.',
+        );
+      } else {
+        toast.error('Erreur lors du téléchargement de la pièce jointe.');
+      }
     } finally {
       setIsMutating(false);
     }
