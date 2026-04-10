1e8bada83be57ee035d3c92c44fc575e311ca9f1
moustaphahennawi <56674781+moustaphahennawi@users.noreply.github.com>
2026-04-10 13:05:39 +0200

    fix: add Attachement type

---
+++ frontend/src/models/attachment.ts
@@ -0,0 +1,10 @@
+export type Attachment = {
+  id: string;
+  charset: string;
+  contentTransferEncoding: string;
+  contentType: string;
+  filename: string;
+  name: string;
+  size: number;
+  file: File;
+};
